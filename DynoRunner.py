a69ac446f532523afb879e36810a21fc51b3d123
ColtonTshudy <82547877+ColtonTshudy@users.noreply.github.com>
2022-12-04 18:02:35 -0500

    Now uses a FSM for control

--- DynoRunner.py
+++ DynoRunner.py
@@ -1,5 +1,5 @@
 __author__ = "Colton Tshudy, Erin Freck"
-__version__ = "0.4"
+__version__ = "0.41"
 __email__ = "coltont@vt.edu"
 __status__ = "Prototyping"
 
@@ -7,23 +7,27 @@ from datetime import datetime
 import serial
 import csv
 import serial.tools.list_ports
+from enum import Enum
 
 #======================================
 # DYNO THROTTLE MAPPER RUNNER
 #======================================
 
+class State(Enum):
+    Idle = 0
+    Executing = 1
+    Finished = 2
+    
 def main():
     '''Executes all commands of a given text file. Records serial to CSV'''
     arduino_port = findArduinoPort()
 
-    #CSV to record to
-    d = open("throttle_log.csv", "a", newline='')
-    writer = csv.writer(d,delimiter=",")
-    formatCSV(writer)
+    #CSV setup
+    writer, d = formatCSV()
 
     #Plaintxt command file
     f = open("throttle_cmds.txt")
-    lines = f.readlines()   
+    lines = f.readlines()
 
     #serial port   
     ser = serial.Serial(port=arduino_port, baudrate=115200, timeout=.1)
@@ -33,9 +37,10 @@ def main():
     curLine = 0
     maxLine = len(lines)
 
-    #read serial, watch for ">"
+    #state variable
+    fsmState = State.Idle
+
     while True:
-        commandExecuting = False
         try:
             #wait until a message is recieved
             if ser.inWaiting() != 0:
@@ -44,32 +49,40 @@ def main():
                 decoded = recieved.decode('ascii')
                 decoded = decoded.strip()
                 print(decoded)
+                msgType = decoded[0]
                 
-                #check what the recieved message contained
-                match decoded[0]:
-                    case '>':
-                        commandExecuting = False
-                        if curLine < maxLine:
+                #fsm to control sending commands
+                match fsmState:
+                    case State.Idle:
+                        if msgType == '>':
                             cmd = lines[curLine]
-                            #append a newline character if needed
                             if cmd[len(cmd)-1] != '\n':
                                 cmd = cmd + "\n"
                             ser.write(cmd.encode('ascii'))
                             curLine = curLine + 1
-                        elif not commandExecuting:
-                            exitProgram()
-                    case '<':
-                        commandExecuting = True
-                    case '[':
-                        decoded = decoded[1:]
-                        data = decoded.split(",")
-                        writer.writerow(data)
+                            fsmState = State.Executing
+
+                    case State.Executing:
+                        if msgType == '<':
+                            if curLine < maxLine:
+                                fsmState = State.Finished
+                            fsmState = State.Idle
+
+                    case State.Finished:
+                        exitProgram()
+
                     case _:
                         pass
+                
+                #record data to CSV
+                if msgType == '[':
+                    decoded = decoded[1:]
+                    data = decoded.split(",")
+                    writer.writerow(data)
             
         # closes all files when user terminates program
         except:
-            ser.write("t 0\n".encode('ascii')) #throttle 0%
+            ser.write("q".encode('ascii')) #send quit command
             d.close
             f.close
             ser.close
@@ -79,28 +92,31 @@ def main():
 # Functions
 #================================
 def findArduinoPort():
+    '''Returns the Arduino Nano port, otherwise returns port COM1'''
     import serial.tools.list_ports
     ports = list(serial.tools.list_ports.comports())
     for p in ports:
         if "CH340" in p.description: #arduino nano tag
             return p.device
     return 'COM1' #default return
-findArduinoPort.__doc__ = "Returns the Arduino Nano port, otherwise returns port COM1"
 
 def exitProgram():
+    '''Prints a message and raises an exception to trigger the catch'''
     softExitMsg = "Reached end of command file"
     print(softExitMsg) 
     raise Exception(softExitMsg)
-exitProgram.__doc__ = "Prints a message and raises an exception to trigger the catch"
 
-def formatCSV(writer):
-    timestamp = datetime.now()
-    date = timestamp.strftime("%m/%d")
-    year = timestamp.strftime("%Y")
-    time = timestamp.strftime("%H:%M:%S")
-    writer.writerow(["New Run", date, year, time])
+def formatCSV():
+    '''Opens a new CSV and writes a header'''
+    timeobj = datetime.now()
+    timestamp = timeobj.strftime("%m_%d_%Y_%I_%M_%p")
+
+    csvName = "".join(["throttle_log_", timestamp, ".csv"])
+    d = open(csvName, 'a', newline='')
+    writer = csv.writer(d,delimiter=",")
+    
     writer.writerow(["Volts", "Thr%", "Ohms", "Millis"])
-exitProgram.__doc__ = "Writes a header to the CSV"
+    return writer, d
 
 # Main Runner
 #==================================
